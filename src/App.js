702443e40b344c614ff17d78d5fc215e5afecac3
seming2 <popqr3@naver.com>
2024-07-01 23:00:33 +0900

    클래스컴포넌트의 사용법과 속성을 이용하여 받아오는법(props)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+class MyNav extends Component{
+  render(){
+    return(
+        <nav>
+          <ul>
+            <li><a href=''>HTML</a> </li>
+            <li><a href=''>CSS</a> </li>
+            <li><a href=''>Javascript</a> </li>
+          </ul>
+        </nav>
+    )
+  }
+}
+
+class Myarticcle extends Component{
+  render(){
+    return(
+        <section>
+          <article>
+            <h2>HTML</h2>
+            <p>Hypertext Markup Language</p>
+          </article>
+        </section>
+    )
+  }
+}
+
+
+/* ES5 문법
+function MyHeader({title}){
+  return (
+    <h1>hello, {title}</h1>
+  )
+}
+*/
+class MyHeader extends Component{
+  render(){
+    return(
+        <header>
+          <h1 className="logo">{this.props.title}</h1>
+          <p>{this.props.desc}</p>
+        </header>
+    )
+  }
+}
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello React!</h1>
+      <div className='App'>
+        <MyHeader title="React" desc="Single Page Application"/>
+        <MyNav/>
+        <Myarticcle/>
 
       </div>
     )
